f623087f8e2f09467f804f3b57f52f6f4137a3f2
anekos <anekos@snca.net>
2019-04-03 21:09:15 +0900

    Sort fns

--- src/command.rs
+++ src/command.rs
@@ -19,9 +19,98 @@ use crate::types::*;
 pub static USAGE: &'static str = include_str!("usage.txt");
 
 
+pub fn check(conn: &Connection, key: &str, value: Option<&str>, ttl: Option<&str>) -> AppResult<bool> {
+    swap_values(conn, key, value, ttl).map(|it| it.is_some())
+}
+
 pub fn get(conn: &Connection, key: &str, default: Option<&str>) -> AppResult<bool> {
     Ok(p(&get_value_with_default(conn, key, default)?))
 }
+
+pub fn has(conn: &Connection, key: &str) -> AppResult<bool> {
+    get_value(conn, key).map(|it| it.is_some())
+}
+
+pub fn import(conn: &Connection, source_path: &str) -> AppResult<bool> {
+    let source_conn = Connection::open(source_path)?;
+
+    let mut stmt = source_conn.prepare("SELECT key, value, created_at, updated_at FROM flags;").unwrap();
+    let entry_iter = stmt.query_map(NO_PARAMS, |row| {
+        Entry {
+            key: row.get(0),
+            value: row.get(1),
+            created_at: row.get(2),
+            updated_at: row.get(3)
+        }
+    }).unwrap();
+
+    let mut result = true;
+    for entry in entry_iter {
+        let entry = entry?;
+        result &= set(conn, &entry.key, entry.value.as_ref().map(String::as_ref), None)?;
+    }
+
+    Ok(result)
+}
+
+pub fn modify(conn: &Connection, key: &str, delta: Option<&str>, minus: bool, ttl: Option<&str>) -> AppResult<bool> {
+    let result = modify_value(conn, key, delta, minus, ttl)?;
+    println!("{}", result);
+    Ok(true)
+}
+
+pub fn remove(conn: &Connection, key: &str) -> AppResult<bool> {
+    let n = conn.execute("DELETE FROM flags WHERE key = ?", &[&key])?;
+    Ok(n == 1)
+}
+
+pub fn set(conn: &Connection, key: &str, value: Option<&str>, ttl: Option<&str>) -> AppResult<bool> {
+    let now = time::get_time();
+    conn.execute(
+        "UPDATE flags SET value = ?, updated_at = ? WHERE key = ?",
+        &[&value as &ToSql, &now as &ToSql, &key]
+    )?;
+    conn.execute(
+        "INSERT INTO flags SELECT ?, ?, ?, ?, NULL WHERE (SELECT changes() = 0)",
+        &[&key, &value as &ToSql, &now, &now]
+    )?;
+
+    set_ttl_opt(conn, key, ttl)?;
+    Ok(true)
+}
+
+pub fn shell(path: &Path, args: Option<&[&str]>) -> AppResult<bool> {
+    let mut command = Command::new("sqlite3");
+    command.arg(path);
+    if let Some(args) = args {
+        command.args(args);
+    }
+    command.exec();
+    Ok(true)
+}
+
+pub fn swap(conn: &Connection, key: &str, value: Option<&str>, ttl: Option<&str>) -> AppResult<bool> {
+    Ok(p(&swap_values(conn, key, value, ttl)?))
+}
+
+pub fn ttl(conn: &Connection, key: &str, ttl: Option<&str>) -> AppResult<bool> {
+    if_let_some!((_, expired_at) = get_value_and_ttl(conn, key)?, Ok(false));
+
+    if let Some(ttl) = ttl {
+        set_ttl(conn, key, ttl)?;
+    } else if let Some(expired_at) = expired_at {
+        let expired_at = expired_at.with_timezone(&Local);
+        println!("{}", expired_at.format("%Y-%m-%d %H:%M:%S"));
+    }
+
+    Ok(true)
+}
+
+pub fn usage() {
+    eprintln!("{}", USAGE);
+}
+
+
 
 #[allow(clippy::option_option)]
 fn get_value(conn: &Connection, key: &str) -> AppResult<Option<Option<String>>> {
@@ -56,31 +145,6 @@ fn get_value_with_default(conn: &Connection, key: &str, default: Option<&str>) -
     }
 }
 
-pub fn has(conn: &Connection, key: &str) -> AppResult<bool> {
-    get_value(conn, key).map(|it| it.is_some())
-}
-
-pub fn set(conn: &Connection, key: &str, value: Option<&str>, ttl: Option<&str>) -> AppResult<bool> {
-    let now = time::get_time();
-    conn.execute(
-        "UPDATE flags SET value = ?, updated_at = ? WHERE key = ?",
-        &[&value as &ToSql, &now as &ToSql, &key]
-    )?;
-    conn.execute(
-        "INSERT INTO flags SELECT ?, ?, ?, ?, NULL WHERE (SELECT changes() = 0)",
-        &[&key, &value as &ToSql, &now, &now]
-    )?;
-
-    set_ttl_opt(conn, key, ttl)?;
-    Ok(true)
-}
-
-pub fn modify(conn: &Connection, key: &str, delta: Option<&str>, minus: bool, ttl: Option<&str>) -> AppResult<bool> {
-    let result = modify_value(conn, key, delta, minus, ttl)?;
-    println!("{}", result);
-    Ok(true)
-}
-
 fn modify_value(conn: &Connection, key: &str, delta: Option<&str>, minus: bool, ttl: Option<&str>) -> AppResult<f64> {
     let delta = delta.as_ref().map(|it| it.parse()).unwrap_or(Ok(1.0))?;
 
@@ -91,96 +155,6 @@ fn modify_value(conn: &Connection, key: &str, delta: Option<&str>, minus: bool, 
     set(conn, key, Some(&format!("{}", modified)), ttl)?;
 
     Ok(modified)
-}
-
-pub fn swap(conn: &Connection, key: &str, value: Option<&str>, ttl: Option<&str>) -> AppResult<bool> {
-    Ok(p(&swap_values(conn, key, value, ttl)?))
-}
-
-#[allow(clippy::option_option)]
-fn swap_values(conn: &Connection, key: &str, value: Option<&str>, ttl: Option<&str>) -> AppResult<Option<Option<String>>> {
-    let result = get_value(conn, key)?;
-    set(conn, key, value, ttl)?;
-    Ok(result)
-}
-
-pub fn check(conn: &Connection, key: &str, value: Option<&str>, ttl: Option<&str>) -> AppResult<bool> {
-    swap_values(conn, key, value, ttl).map(|it| it.is_some())
-}
-
-pub fn import(conn: &Connection, source_path: &str) -> AppResult<bool> {
-    let source_conn = Connection::open(source_path)?;
-
-    let mut stmt = source_conn.prepare("SELECT key, value, created_at, updated_at FROM flags;").unwrap();
-    let entry_iter = stmt.query_map(NO_PARAMS, |row| {
-        Entry {
-            key: row.get(0),
-            value: row.get(1),
-            created_at: row.get(2),
-            updated_at: row.get(3)
-        }
-    }).unwrap();
-
-    let mut result = true;
-    for entry in entry_iter {
-        let entry = entry?;
-        result &= set(conn, &entry.key, entry.value.as_ref().map(String::as_ref), None)?;
-    }
-
-    Ok(result)
-}
-
-pub fn shell(path: &Path, args: Option<&[&str]>) -> AppResult<bool> {
-    let mut command = Command::new("sqlite3");
-    command.arg(path);
-    if let Some(args) = args {
-        command.args(args);
-    }
-    command.exec();
-    Ok(true)
-}
-
-pub fn remove(conn: &Connection, key: &str) -> AppResult<bool> {
-    let n = conn.execute("DELETE FROM flags WHERE key = ?", &[&key])?;
-    Ok(n == 1)
-}
-
-pub fn ttl(conn: &Connection, key: &str, ttl: Option<&str>) -> AppResult<bool> {
-    if_let_some!((_, expired_at) = get_value_and_ttl(conn, key)?, Ok(false));
-
-    if let Some(ttl) = ttl {
-        set_ttl(conn, key, ttl)?;
-    } else if let Some(expired_at) = expired_at {
-        let expired_at = expired_at.with_timezone(&Local);
-        println!("{}", expired_at.format("%Y-%m-%d %H:%M:%S"));
-    }
-
-    Ok(true)
-}
-
-fn set_ttl(conn: &Connection, key: &str, ttl: &str) -> AppResultU {
-    let expired_at = parse_expired_at(ttl)?;
-
-    let updated = conn.execute(
-        "UPDATE flags SET expired_at = ? WHERE key = ?",
-        &[&expired_at as &ToSql, &key]
-    )?;
-    if updated != 1 {
-        panic!("WTF!");
-    }
-    Ok(())
-}
-
-fn set_ttl_opt(conn: &Connection, key: &str, ttl: Option<&str>) -> AppResultU {
-    if let Some(ttl) = ttl {
-        set_ttl(conn, key, ttl)?;
-    }
-    Ok(())
-}
-
-
-pub fn usage() {
-    eprintln!("{}", USAGE);
 }
 
 #[allow(clippy::option_option)]
@@ -211,4 +185,31 @@ fn parse_expired_at(s: &str) -> AppResult<DateTime<Local>> {
             let expired_at: SystemTime = now + ttl;
             Ok(expired_at.into())
         })
+}
+
+fn set_ttl(conn: &Connection, key: &str, ttl: &str) -> AppResultU {
+    let expired_at = parse_expired_at(ttl)?;
+
+    let updated = conn.execute(
+        "UPDATE flags SET expired_at = ? WHERE key = ?",
+        &[&expired_at as &ToSql, &key]
+    )?;
+    if updated != 1 {
+        panic!("WTF!");
+    }
+    Ok(())
+}
+
+fn set_ttl_opt(conn: &Connection, key: &str, ttl: Option<&str>) -> AppResultU {
+    if let Some(ttl) = ttl {
+        set_ttl(conn, key, ttl)?;
+    }
+    Ok(())
+}
+
+#[allow(clippy::option_option)]
+fn swap_values(conn: &Connection, key: &str, value: Option<&str>, ttl: Option<&str>) -> AppResult<Option<Option<String>>> {
+    let result = get_value(conn, key)?;
+    set(conn, key, value, ttl)?;
+    Ok(result)
 }
